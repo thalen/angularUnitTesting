2b70bca2d0ba40473dfa9a29fe80061b7c05bddb
Olle Thalén <olle.thalen@omegapoint.se>
2014-10-15 20:24:36 +0200

    test test

--- app/modules/start/controllers/StartController.js
+++ app/modules/start/controllers/StartController.js
@@ -15,7 +15,7 @@ angular.module('unittestDemo.start')
             var filteredList = _.map( _.filter(response, function (elem) {
                 return elem.subject === $scope.dataModel.subject;
             }), function (elem) {
-               return elem.name.toLowerCase();
+               return elem.name;
             });
 
             $scope.dataModel.courses = filteredList.join('\n');
